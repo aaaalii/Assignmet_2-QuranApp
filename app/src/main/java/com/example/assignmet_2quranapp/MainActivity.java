62614ffd449a8cffdb0ceca460d975b5c401b813
aliShykh20 <ahmadmubashar373@gmail.com>
2023-06-04 10:01:00 -0700

    Second Commit

--- app/src/main/java/com/example/assignmet_2quranapp/MainActivity.java
+++ app/src/main/java/com/example/assignmet_2quranapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.assignmet_2quranapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView lv = findViewById(R.id.surahsView);
+        ArrayList<String>
     }
 }
